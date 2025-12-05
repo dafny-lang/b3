8d05b24f453fc10c9c077728be8886481f8800f0
Rustan Leino <leino@amazon.com>
2025-12-04 17:14:34 -0800

    chore: Change indentation to match the pygment file

--- doc/refman/_static/inline-highlight.js
+++ doc/refman/_static/inline-highlight.js
@@ -1,51 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Note, these reserved words are duplicated in doc/refman/b3_pygments.py
-    const b3Keywords = [
-      // top-level declarations
-      'type',
-      'tagger',
-      'function',
-      'axiom',
-      'procedure',
-      // statements
-      'var',
-      'val',
-      'reinit',
-      'exit',
-      'return',
-      'check',
-      'assume',
-      'reach',
-      'assert',
-      'probe',
-      'forall',
-      'choose', 'or',
-      'if', 'else', 'case',
-      'loop',
-      // expressions
-      'then',
-      'exists',
-      'false',
-      'true',
-      'old',
-      'div',
-      'mod',
-      // types
-      'bool',
-      'int',
-      // modifiers
-      'for',
-      'injective', 'tag', 'when',
-      'explains',
-      'inout', 'out', 'requires', 'ensures',
-      'invariant',
-      'pattern',
-      'autoinv',
-    ];
+  // Note, these reserved words are duplicated in doc/refman/b3_pygments.py
+  const b3Keywords = [
+    // top-level declarations
+    'type',
+    'tagger',
+    'function',
+    'axiom',
+    'procedure',
+    // statements
+    'var',
+    'val',
+    'reinit',
+    'exit',
+    'return',
+    'check',
+    'assume',
+    'reach',
+    'assert',
+    'probe',
+    'forall',
+    'choose', 'or',
+    'if', 'else', 'case',
+    'loop',
+    // expressions
+    'then',
+    'exists',
+    'false',
+    'true',
+    'old',
+    'div',
+    'mod',
+    // types
+    'bool',
+    'int',
+    // modifiers
+    'for',
+    'injective', 'tag', 'when',
+    'explains',
+    'inout', 'out', 'requires', 'ensures',
+    'invariant',
+    'pattern',
+    'autoinv',
+  ];
 
-    document.querySelectorAll('code.docutils.literal.notranslate span.pre').forEach(function(span) {
-        if (b3Keywords.includes(span.textContent.trim())) {
-            span.classList.add('b3-keyword');
-        }
-    });
+  document.querySelectorAll('code.docutils.literal.notranslate span.pre').forEach(function(span) {
+      if (b3Keywords.includes(span.textContent.trim())) {
+          span.classList.add('b3-keyword');
+      }
+  });
 });
